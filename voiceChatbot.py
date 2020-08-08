848cbe0caf76e9300dc3994a2d91cd89e11ca5e6
Amolik Vivian Paul <45116992+amolikvivian@users.noreply.github.com>
2020-08-09 04:19:54 +0530

    Add files via upload

---
+++ voiceChatbot.py
@@ -0,0 +1,134 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+import pickle
+import speech_recognition as sr
+
+with open("dataset/dataset.json") as file:
+    data = json.load(file)
+
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=300, batch_size=8, show_metric=True)
+model.save("model.tflearn")
+
+print('[INFO] Model successfully trained')
+print('[INFO] ...')
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+            
+    return numpy.array(bag)
+
+
+def chat():
+    print("Start talking with the bot (type quit to stop)!")
+    
+    r = sr.Recognizer()
+
+    while True:
+
+        try:
+            with sr.Microphone() as source:
+                
+                r.adjust_for_ambient_noise(source, duration=0.2)
+                inputAudio = r.listen(source)
+
+                inputText = r.recognize_google(inputAudio)
+                inputText = inputText.lower()
+
+                print('You:', inp)
+                if inp.lower() == "quit":
+                    print('Bye!')
+                    break
+
+            results = model.predict([bag_of_words(inputText, words)])
+            results_index = numpy.argmax(results)
+            tag = labels[results_index]
+
+            for tg in data["intents"]:
+                if tg['tag'] == tag:
+                    responses = tg['responses']
+
+            print(random.choice(responses))
+
+        except:
+            print('Sorry, I could not understand what you said, please try again')
+            print('[INFO] Speak again...')
+
+chat()
